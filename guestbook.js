b6d3db820b8bd0edfc224db69e8c6572194a4d96
Munday <derek.b.munday@outlook.com>
2022-04-12 16:24:12 -0400

    added API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6dd06fff.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
